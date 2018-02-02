402f1395a526d6d81e01c2757d69702c1bf1ec83
lining <lin@yoga>
2018-02-02 15:54:48 +0800

    20180202

--- InterviewQuestions/Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
+++ InterviewQuestions/Print1ToMaxOfNDigits/Print1ToMaxOfNDigits.cpp
@@ -105,35 +105,18 @@ void PrintNumber(char* number)
     bool isBeginning0 = true;
     int nLength = strlen(number);
  
-#if 0   //原来的代码
     for(int i = 0; i < nLength; ++ i)
     {
-		if (isBeginning0 && number[i] != '0')
-			isBeginning0 = false;
-
+        if(isBeginning0 && number[i] != '0')
+            isBeginning0 = false;
+ 
         if(!isBeginning0)
         {
             printf("%c", number[i]);
         }
     }
-	printf("%s", number+ not0index);
+ 
     printf("\t");
-#endif
-
-	int not0index = 0;
-	for (int i = 0; i < nLength; ++i)
-	{
-		if (isBeginning0 && number[i] != '0')
-		{
-			not0index = i;
-			break;
-		}
-	}	
-	if (number[not0index] == '0')
-		printf("0");
-	else
-		printf("%s", number + not0index);
-	printf("\t");
 }
 
 // ====================测试代码====================
@@ -141,7 +124,7 @@ void Test(int n)
 {
     printf("Test for %d begins:\n", n);
 
-    //Print1ToMaxOfNDigits_1(n);
+    Print1ToMaxOfNDigits_1(n);
     Print1ToMaxOfNDigits_2(n);
 
     printf("Test for %d ends.\n", n);
@@ -149,13 +132,12 @@ void Test(int n)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-   // Test(1);
+    Test(1);
     Test(2);
-    //Test(10);
-    //Test(0);
-    //Test(-1);
+    Test(3);
+    Test(0);
+    Test(-1);
 
-	getchar();
     return 0;
 }
 
